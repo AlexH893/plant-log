e902518c01508f7230ea8e55466fa5d34b3402d1
Alex <31377763+AlexH893@users.noreply.github.com>
2025-03-13 16:45:18 -0600

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://plant-log.onrender.com/api',
   plantCountUrl: 'https://plant-log.onrender.com/api/total-plants',
-  // apiUrl: 'http://localhost:3000/api',
+  userUrl: 'https://plant-log.onrender.com/api/user',
 };
