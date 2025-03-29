29ab9314b5b12c30f7510efd81e29d68d44078fb
Alex <31377763+AlexH893@users.noreply.github.com>
2025-03-29 15:59:52 -0600

    commit

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 const mysql = require("mysql2/promise");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const axios = require("axios");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+import { environment } from "src/environments/environment"; // Generic import
 // Prod
 const db = mysql.createPool({
   host: process.env.DB_HOST,
@@ -33,6 +33,29 @@ app.use(cors());
 module.exports = db;
 const routes = require("./routes");
 app.use("/api", routes);
+
+// Keep-alive route
+app.get("/keep-alive", (req, res) => {
+  res.status(200).send("Server is alive!");
+});
+
+// Function to get a timestamp
+const getTimestamp = () => {
+  return new Date().toLocaleString(); // Formats the date & time
+};
+
+// Self-pinging function
+const pingServer = async () => {
+  try {
+    await axios.get(`https://plant-log.onrender.com/api/keep-alive`);
+    console.log(`[${getTimestamp()}] Self keep-alive ping sent`);
+  } catch (error) {
+    console.error(`[${getTimestamp()}] Keep-alive error:`, error.message);
+  }
+};
+
+// Set interval to call keep-alive every 8 minutes
+setInterval(pingServer, 8 * 60 * 1000); // 8 minutes
 
 const port = 3000;
 app.listen(port, () => {
