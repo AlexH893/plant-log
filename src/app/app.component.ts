7a5164770bf914c9671de9a88247faa2d6616035
Alex <31377763+AlexH893@users.noreply.github.com>
2025-07-23 18:21:49 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  constructor(private authService: AuthService) {}
   title = 'plant-log';
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      const user = this.authService.getUserInfo();
+      console.log('User still authenticated:', user);
+      // store user in a shared service or app state
+    } else {
+      console.log('User not authenticated or token expired.');
+      this.authService.logout(); // clear any old token
+    }
+  }
 }
